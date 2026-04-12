703a336adc89e7fe851527e59350fc233d63a7a3
rohksattra <rohksattra@gmail.com>
2026-04-12 20:55:31 +0700

    Add files via upload

--- app/uis/claim_embed.py
+++ app/uis/claim_embed.py
@@ -9,36 +9,41 @@ ClaimAction = Literal["claim", "unclaim", "force_claim", "force_unclaim"]
 
 
 def claim_log_embed(
-    *, worker: discord.Member, item_name: str, quantity: int, channel: discord.TextChannel, action: ClaimAction, staff: discord.Member | None = None,
+    *, worker: discord.Member, item_name: str, quantity: int, channel: discord.TextChannel,
+    action: ClaimAction, staff: discord.Member | None = None, item_emoji: str = "",
 ) -> discord.Embed:
+
+    item_emoji = item_emoji or "🌟"
+
     if action == "claim":
         description = (
             f"***{worker.mention}*** has claimed "
-            f"🏷 ***{quantity:,}x*** of ***{item_name}*** "
+            f"🏷 ***{quantity:,}x {item_emoji} {item_name}*** "
             f"in ***{channel.mention}***"
         )
     elif action == "unclaim":
         description = (
             f"***{worker.mention}*** has unclaimed "
-            f"🏷 ***{quantity:,}x*** of ***{item_name}*** "
+            f"🏷 ***{quantity:,}x {item_emoji} {item_name}*** "
             f"in ***{channel.mention}***"
         )
     elif action == "force_claim":
         staff_mention = staff.mention if staff else "**Staff**"
         description = (
             f"***{staff_mention}*** forced ***{worker.mention}*** to claim "
-            f"🏷 ***{quantity:,}x*** of ***{item_name}*** "
+            f"🏷 ***{quantity:,}x {item_emoji} {item_name}*** "
             f"in ***{channel.mention}***"
         )
     elif action == "force_unclaim":
         staff_mention = staff.mention if staff else "**Staff**"
         description = (
             f"***{staff_mention}*** forced ***{worker.mention}*** to unclaim "
-            f"🏷 ***{quantity:,}x*** of ***{item_name}*** "
+            f"🏷 ***{quantity:,}x {item_emoji} {item_name}*** "
             f"in ***{channel.mention}***"
         )
     else:
         description = "Unknown claim action."
+
     embed = discord.Embed(
         title="📌 Order Claim Update",
         description=description,
